c3b4f4eac0fa60d1fba2c8602c1665fbd3c29a5e
erikmooney <erik@dos486.com>
2013-08-12 00:06:47 -0400

    Adding prerendered video file for city world, and code to play it

--- SecondRealipony/SecondRealipony/World.cs
+++ SecondRealipony/SecondRealipony/World.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
 
 namespace SecondRealipony
 {
@@ -12,20 +13,26 @@ namespace SecondRealipony
         public override float EndBeat { get { return 106; } }
         public override string MusicName { get { return "World.wav"; } }
         public override float BeatLength { get { return 60F / 125F; } }
-
-        Texture2D placeholder;
+        VideoPlayer videoPlayer;
+        Video video;
 
         public World(Game game)
             : base(game)
         {
-            placeholder = game.Content.Load<Texture2D>("placeholder.jpg");
+            video = game.Content.Load<Video>("World.wmv");
+            videoPlayer = new VideoPlayer();
         }
 
         protected override void DrawSegment()
         {
+            if (videoPlayer.State == MediaState.Stopped)
+            {
+                videoPlayer.Play(video);
+            }
+
             var batch = new SpriteBatch(device);
             batch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied);
-            batch.Draw(placeholder, ScreenCenter, null, Color.White, 0, placeholder.Center(), 0.5F, SpriteEffects.None, 0);
+            batch.Draw(videoPlayer.GetTexture(), FullScreen, Color.White);
             batch.End();
         }
     }
